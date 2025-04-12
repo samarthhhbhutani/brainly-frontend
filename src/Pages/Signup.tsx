714994a0f92b3d4aa4e0f2ac371ec433a61996f1
Samarth <bhutani.samarth@gmail.com>
2025-04-12 19:29:21 +0000

    Changes

--- src/Pages/Signup.tsx
+++ src/Pages/Signup.tsx
@@ -74,9 +74,20 @@ export function Signup() {
                         {usernameError && <span className="text-red-500 ">{usernameError}</span>}
                         <Input handeClick={()=>{if(!usernameError && !passwordError){buttonRef.current?.click()}}} ref={passwordRef} onCh={(e) => validatePassword(e.target.value)} placeholder="Password" />
                         {passwordError && <span className="text-red-500">{passwordError}</span>}
-                        <div className="flex justify-center py-4 w-full">
+                        <div className="flex justify-center pb-1 pt-4 w-full">
                             <Button loading={!usernameError && !passwordError ? false : true} refer={buttonRef} variant="primary" text="Sign up" onClick={signup} fullWidth={true}></Button>
                         </div>
+                        <div className="text-white flex items-center justify-center font-semibold text-md">Or</div>
+                <div className="flex pt-1 justify-center pb-4 w-full">
+                    <form className="w-full" action={BACKEND_URL+"/auth/google"} method="GET" target="_self">
+                    <button className="px-4 py-5 rounded-md font-light justify-between cursor-pointer w-full hover:opacity-80 bg-white flex items-center h-8" type="submit" id="login-with-google-btn" >
+                        <img className="" src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj48cGF0aCBkPSJNMTcuNiA5LjJsLS4xLTEuOEg5djMuNGg0LjhDMTMuNiAxMiAxMyAxMyAxMiAxMy42djIuMmgzYTguOCA4LjggMCAwIDAgMi42LTYuNnoiIGZpbGw9IiM0Mjg1RjQiIGZpbGwtcnVsZT0ibm9uemVybyIvPjxwYXRoIGQ9Ik05IDE4YzIuNCAwIDQuNS0uOCA2LTIuMmwtMy0yLjJhNS40IDUuNCAwIDAgMS04LTIuOUgxVjEzYTkgOSAwIDAgMCA4IDV6IiBmaWxsPSIjMzRBODUzIiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNNCAxMC43YTUuNCA1LjQgMCAwIDEgMC0zLjRWNUgxYTkgOSAwIDAgMCAwIDhsMy0yLjN6IiBmaWxsPSIjRkJCQzA1IiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNOSAzLjZjMS4zIDAgMi41LjQgMy40IDEuM0wxNSAyLjNBOSA5IDAgMCAwIDEgNWwzIDIuNGE1LjQgNS40IDAgMCAxIDUtMy43eiIgZmlsbD0iI0VBNDMzNSIgZmlsbC1ydWxlPSJub256ZXJvIi8+PHBhdGggZD0iTTAgMGgxOHYxOEgweiIvPjwvZz48L3N2Zz4=">
+                        </img>
+                        <div className="">Sign in with Google</div>
+                        <div></div>
+                    </button>
+                    </form>
+                </div>
                         {error && <div className="text-red-500 text-center mb-2 text-lg">{error}</div>}
                         <h2 className="flex justify-center text-sm grow-1 shrink-0 basis-auto w-full mx-auto text-white hover:opacity-80">
                             Already have an account? <span className="ml-1 cursor-pointer underline font-bold text-purple-600" onClick={() => { navigate("/signin") }}>Sign in</span>
--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export const BACKEND_URL="http://localhost:3000"
+export const BACKEND_URL="https://brainlybackend-7fcv.onrender.com"
